29fa70a2d522b0fb7d37fcd7ab71a7a7cd8a925f
Akın Cengiz <akincengiz@msn.com>
2023-07-20 15:58:02 +0300

    .

--- DrawARectangle/DrawARectangle/Program.cs
+++ DrawARectangle/DrawARectangle/Program.cs
@@ -1,46 +1,63 @@
-﻿namespace DrawARectangle
+﻿using System.Security.Principal;
+
+namespace DrawARectangle
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Dikdörtgenin Genişliği\t\t: ");
-            int width = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Dikdörtgenin Yüksekliği\t\t: ");
-            int height = Convert.ToInt32(Console.ReadLine());
-            bool isFill = false;
-            Console.WriteLine("Dikdörgenin içi dolu mu boş mu\t?");
-            Console.Write("Dolu ise ( 1 )\tBoş ise ( 0 )\t:");
-            string inputValue = Console.ReadLine();
-            isFill = (inputValue == "1") ? true : false;
-            Console.WriteLine();
-
-            for (int i = 0; i < height ; i++)
+            while (true)
             {
-                for (int j = 0; j < width ; j++)
+                Console.Write("Dikdörtgenin Genişliği\t\t: ");
+                int width = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Dikdörtgenin Yüksekliği\t\t: ");
+                int height = Convert.ToInt32(Console.ReadLine());
+                bool isFill = false;
+                Console.WriteLine("Dikdörgenin içi dolu mu boş mu\t?");
+                Console.Write("Dolu ise ( 1 )\tBoş ise ( 0 )\t:");
+                string inputValue = Console.ReadLine();
+                isFill = (inputValue == "1") ? true : false;
+                Console.WriteLine();
+
+                for (int i = 0; i < height; i++)
                 {
-                    if (i + j == j || i + j == i || i == height - 1  || j == width - 1)
+                    for (int j = 0; j < width; j++)
                     {
-                        Console.Write(" * ");
-                    }
-                    else
-                    {
-                        if (isFill)
+                        if (i + j == j || i + j == i || i == height - 1 || j == width - 1)
                         {
                             Console.Write(" * ");
                         }
                         else
                         {
-                            Console.Write("   ");
-                        }
-                        
-                    }
-                }
-                Console.WriteLine();
-            }
+                            if (isFill)
+                            {
+                                Console.Write(" * ");
+                            }
+                            else
+                            {
+                                Console.Write("   ");
+                            }
 
-            string statu = isFill ? "Dolu" : "Boş";
-            Console.WriteLine("\n---------------------------------\nGenişliği\t: {0,3}\nYüksekliği\t: {1,3}\nolan içi {2,4} dikdörtgen...",width,height,statu);
+                        }
+                    }
+                    Console.WriteLine();
+                }
+
+                string statu = isFill ? "Dolu" : "Boş";
+                Console.WriteLine("\n---------------------------------\nGenişliği\t: {0,3}\nYüksekliği\t: {1,3}\nolan içi {2,4} dikdörtgen...", width, height, statu);
+                Console.WriteLine("Devam etmek için e tuşuna çıkış için herhangi bir tuşa basınız...");
+                string choice = Console.ReadLine();
+                if (choice.ToLower() == "e")
+                {
+                    Console.Clear();
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
         }
     }
 }
