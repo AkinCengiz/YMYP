847771c22c5f774be0ed28f7d323157ff2cdb157
Akın Cengiz <akincengiz@msn.com>
2023-07-20 15:21:33 +0300

    Console da içi dolu yada boş diktörtgen uygulaması

---
+++ DrawARectangle/DrawARectangle/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace DrawARectangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dikdörtgenin Genişliği\t\t: ");
+            int width = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Dikdörtgenin Yüksekliği\t\t: ");
+            int height = Convert.ToInt32(Console.ReadLine());
+            bool isFill = false;
+            Console.WriteLine("Dikdörgenin içi dolu mu boş mu\t?");
+            Console.Write("Dolu ise ( 1 )\tBoş ise ( 0 )\t:");
+            string inputValue = Console.ReadLine();
+            isFill = (inputValue == "1") ? true : false;
+            Console.WriteLine();
+
+            for (int i = 0; i < height ; i++)
+            {
+                for (int j = 0; j < width ; j++)
+                {
+                    if (i + j == j || i + j == i || i == height - 1  || j == width - 1)
+                    {
+                        Console.Write(" * ");
+                    }
+                    else
+                    {
+                        if (isFill)
+                        {
+                            Console.Write(" * ");
+                        }
+                        else
+                        {
+                            Console.Write("   ");
+                        }
+                        
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            string statu = isFill ? "Dolu" : "Boş";
+            Console.WriteLine("\n---------------------------------\nGenişliği\t: {0,3}\nYüksekliği\t: {1,3}\nolan içi {2,4} dikdörtgen...",width,height,statu);
+        }
+    }
+}
---
+++ DrawATriangle/DrawATriangle/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace DrawATriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Üçgenin yüksekliği : ");
+            int step = Convert.ToInt32(Console.ReadLine());
+
+            int[,] triangle = new int[step,step * 2 + 1];
+            for (int i = 0; i < step; i++)
+            {
+                for (int j = 0; j < step * 2 - 1; j++)
+                {
+                    if (i == 0)
+                    {
+                        triangle[i,j] = 1;
+                    }
+                    if (i == j)
+                    {
+                        triangle[i,j] = 1;
+                    }
+                    if (j == step - 1)
+                    {
+                        triangle[i,j] = 1;
+                    }
+                    if ((i <= step - 1 && j > step - 1) && (j + i > step - 1 && j + i <= (step - 1) * 2))
+                    {
+                        triangle[i,j] = 1;
+                    }
+                    if ((j >= i && j < step - 1) && (j - i > 0 && j - i < step - 1))
+                    {
+                        triangle[i,j] = 1;
+                    }
+                }
+            }
+            for (int i = 0; i < step; i++)
+            {
+                for (int j = 0; j < step * 2 - 1; j++)
+                {
+                    if (triangle[i,j] == 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
