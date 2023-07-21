3fd4d9eeeb4d4db27cd0e4ed6c88e8d8c159d507
Akın Cengiz <akincengiz@msn.com>
2023-07-21 12:03:20 +0300

    .

---
+++ TriangleFillAndEmpty/TriangleFillAndEmpty/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace TriangleFillAndEmpty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Üçgenin taban uzunluğu : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                a++;
+            }
+            Console.WriteLine("======================================\n\n");
+            int[,] triangle = new int[a, a];
+            
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < a; j++)
+                {
+                    if ((i + j) == a / 2)
+                    {
+                        triangle[i, j] = 1;
+                    }
+
+                    if ((j - i) == a / 2)
+                    {
+                        triangle[i, j] = 1;
+                    }
+
+                    if (i == a / 2)
+                    {
+                        triangle[i, j] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i <= triangle.GetUpperBound(0); i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j <= triangle.GetUpperBound(1); j++)
+                {
+                    if (triangle[i, j] == 1)
+                    {
+                        Console.Write("{0,3}", "*");
+                    }
+                    else
+                    {
+                        Console.Write("{0,3}", " ");
+                    }
+                }
+            }
+        }
+    }
+}
